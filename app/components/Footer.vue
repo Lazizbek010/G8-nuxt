64074d91ba92f13525e828ae692eb16c6c9bf58e
shkrtbkva <shukhratbekovaa@gmail.com>
2025-10-19 12:18:22 +0500

    Footer

--- app/components/Footer.vue
+++ app/components/Footer.vue
@@ -1,6 +1,83 @@
 <template>
-  <div>
-    Footer
+  <div class="bg-[#0d2235] text-white py-10">
+ <div class="max-w-7xl mx-auto px-4 grid grid-cols-1 md:grid-cols-4 gap-8">
+      <!-- Contact Us -->
+      <div>
+        <h3 class="text-lg font-semibold mb-4">Contact Us</h3>
+        <ul class="space-y-3 text-sm text-gray-300">
+          <li class="flex items-start gap-2">
+              <Icon name="Location"></Icon>
+            <span>329 Queensberry Street, North Melbourne VIC 3051, Australia.</span>
+          </li>
+          <li class="flex items-center gap-2">
+            <span>
+              <Icon name="Phone"></Icon>
+            </span>
+            <span>123 456 7890</span>
+          </li>
+          <li class="flex items-center gap-2">
+            <span>
+              <Icon name="Email"></Icon>
+              
+            </span>
+            <span>support@houzing.com</span>
+          </li>
+          <li class="flex gap-2 mt-4">
+          <Icon name="Face" class="px-3 py-2 rounded-md hover:bg-white/20"></Icon> 
+          <Icon name="Twiter" class="px-3 py-2 rounded-md hover:bg-white/20"></Icon> 
+          <Icon name="Insta" class="px-3 py-2 rounded-md hover:bg-white/20"></Icon> 
+          <Icon name="In" class="px-3 py-2 rounded-md hover:bg-white/20"></Icon> 
+          </li>
+        </ul>
+      </div>
+
+      <!-- Discover -->
+      <div>
+        <h3 class="text-lg font-semibold mb-4">Discover</h3>
+        <ul class="space-y-3 text-sm text-gray-300">
+          <li><a href="#">Chicago</a></li>
+          <li><a href="#">Los Angeles</a></li>
+          <li><a href="#">Miami</a></li>
+          <li><a href="#">New York</a></li>
+        </ul>
+      </div>
+
+      <!-- Lists by Category -->
+      <div>
+        <h3 class="text-lg font-semibold mb-4">Lists by Category</h3>
+        <ul class="space-y-3 text-sm text-gray-300">
+          <li><a href="#">Apartments</a></li>
+          <li><a href="#">Condos</a></li>
+          <li><a href="#">Houses</a></li>
+          <li><a href="#">Offices</a></li>
+          <li><a href="#">Retail</a></li>
+          <li><a href="#">Villas</a></li>
+        </ul>
+      </div>
+
+      <!-- Other Links -->
+      <div>
+        <h3 class="text-lg font-semibold mb-4">Lists by Category</h3>
+        <ul class="space-y-3 text-sm text-gray-300">
+          <li><a href="#">About Us</a></li>
+          <li><a href="#">Terms & Conditions</a></li>
+          <li><a href="#">Support Center</a></li>
+          <li><a href="#">Contact Us</a></li>
+        </ul>
+      </div>
+    </div>
+
+    <!-- Bottom -->
+    <div class="mt-10 border-t border-gray-700 pt-6 text-center text-sm text-gray-400 flex justify-between items-center px-4 max-w-9xl mx-auto">
+      <div class="flex items-center gap-2">
+        <span> <Icon name="Logo"></Icon> </span>
+        <span class="font-semibold text-white">Houzing</span>
+      </div>
+      <div>
+        Copyright © 2021 CreativeLayers. All Right Reserved.
+      </div>
+      
+    </div>
   </div>
 </template>
 
