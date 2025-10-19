bc97ad3182cd1419a28b55a90bd0f7c208f8292c
Lazizbek010 <lazizbekisaqov19981110@gmail.com>
2025-10-19 11:12:54 +0500

    index sahifasi

---
+++ app/pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <section class="swiper">SWIPER</section>
+    <section class="choose">CHOOSE</section>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
